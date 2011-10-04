a1ffed1f15cbfa04eac615ef17e0aaae500c8db6
quidmonkey <quidmonkey@gmail.com>
2011-10-04 14:29:33 -0300

    Edited zmagic.js via GitHub

--- zmagic.js
+++ zmagic.js
@@ -25,47 +25,48 @@ ig.Zmagic = ig.Class.extend({
     		//Inject three new methods into ig.Entity
 		ig.Entity.inject({
 			
-		//Move calling Entity to the Back
-		moveToBack: function(){
-			var ents = ig.game.entities;
-			ents.splice( ents.indexOf( this ), 1 );
-			ents.unshift( this );
-		},
-        	
-        	//Move calling Entity to the front
-            	moveToFront: function() {
-            		var ents = ig.game.entities;
-                	ents.splice( ents.indexOf( this ), 1 );
-                	ents.push( this );
-            	},
-        	
-        	//Move calling Entity to a specified zIndex
-	        setzIndex: function( newZ ){
-	        	
-	        	//remove current zIndex
-	    		var ents = ig.game.entities;
-	    		ents.splice( ents.indexOf( this ), 1 );
-	    		
-	    		//insert new zIndex
-	    		var i = ents.length;
-	    		while( i-- ){
-			        if( ents[i].zIndex <= newZ ){
-			            ents.splice( i + 1, 0, this );
-			            break;
-			        }
-	    		}
-	    		this.zIndex = newZ;
+			//Move calling Entity to the Back
+			moveToBack: function(){
+				var ents = ig.game.entities;
+				ents.splice( ents.indexOf( this ), 1 );
+				ents.unshift( this );
 			},
-			
+	        	
+	        	//Move calling Entity to the front
+	            	moveToFront: function() {
+	            		var ents = ig.game.entities;
+	                	ents.splice( ents.indexOf( this ), 1 );
+	                	ents.push( this );
+	            	},
+	        	
+	        	//Move calling Entity to a specified zIndex
+		        setzIndex: function( newZ ){
+		        	
+		        	//remove current zIndex
+		    		var ents = ig.game.entities;
+		    		ents.splice( ents.indexOf( this ), 1 );
+		    		
+		    		//insert new zIndex
+		    		var i = ents.length;
+		    		while( i-- ){
+				        if( ents[i].zIndex <= newZ ){
+				            ents.splice( i + 1, 0, this );
+				            break;
+				        }
+		    		}
+		    		this.zIndex = newZ;
+			},
+				
 			//Swap calling Entity with another Entity
 			swapzIndex: function( ent ){
-				var ents = ig.game.entities;
-				var a = ents.indexOf( this );
-				var b = ents.indexOf( ent );
-				ents[a] = ent;
-				ents[b] = this;
+					var ents = ig.game.entities;
+					var a = ents.indexOf( this );
+					var b = ents.indexOf( ent );
+					ents[a] = ent;
+					ents[b] = this;
 			}
-        	});
+		
+		});
 
         	ig.Zmagic.instance = this;
     	}
